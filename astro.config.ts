72fac66663804097621dce84acba94edf5f49cb5
JeanFi675 <duheronjp@gmail.com>
2026-01-02 13:13:13 +0100

    feat: SEO configuration.

--- astro.config.ts
+++ astro.config.ts
@@ -1,10 +1,15 @@
 import { defineConfig } from 'astro/config';
 
+import sitemap from '@astrojs/sitemap';
+
 export default defineConfig({
   output: 'static',
   site: 'https://fjd2026.caflarochebonneville.fr',
   base: '/',
+
   build: {
     inlineStylesheets: 'auto',
   },
+
+  integrations: [sitemap()],
 });
